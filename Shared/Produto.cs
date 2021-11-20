a1b0b60f0bdca10db9d3c166a07de902a0101be2
renato_sans <renato@datacopy.com.br>
2021-11-20 10:14:45 -0300

    Inclusão do template mínimo do projeto

---
+++ Shared/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace wasmSample
+{
+    // using System.Text.Json.Serialization;
+
+    public class Produto
+    {
+        public int id { get; set; }
+        public String nome { get; set; }
+        public String descricao { get; set; }
+        public Double preco { get; set; }
+        public String imagem { get; set; }
+    }
+}
---
+++ Shared/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace wasmSample
+{
+    public class ShoppingCart
+    {
+        public String nomeCliente;
+        public List<Produto> produtosAdicionados;
+        public Double desconto;
+
+        public ShoppingCart(String nomeCliente)
+        {
+            this.nomeCliente = nomeCliente;
+            produtosAdicionados = new List<Produto>();
+        }
+        internal void adicionar(Produto produto)
+        {
+            if (produto is not null) 
+                produtosAdicionados.Add(produto);
+        }
+    }
+}
