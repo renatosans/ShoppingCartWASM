9a8109d8175af14388dda0d03d909d277867f083
renato_sans <renato@datacopy.com.br>
2022-04-30 12:51:24 -0300

    Inclusão de lib de componentes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wasmSample;
+using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazorStrap();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
