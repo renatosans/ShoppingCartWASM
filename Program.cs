0591f5eafd67b94a365d52ef3829ba1fa52dcd34
renato_sans <renato@datacopy.com.br>
2022-05-03 07:47:17 -0300

    Substituição da lib de componentes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wasmSample;
-using BlazorStrap;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazorStrap();
+builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
