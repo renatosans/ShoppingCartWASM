9485311f318c3a1295d1a476b38ed42d8a6fb793
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-08-10 04:17:26 -0300

    Ajustes

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.Extensions.FileProviders;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+/*
+var provider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Public"));
+provider.Watch("*.*");
+
+var options = new StaticFileOptions()
+{
+    RequestPath = "",
+    FileProvider = provider
+};
+app.UseStaticFiles(options);
+*/
+
+app.MapGet("/", () => "Carregando produtos...");
 
 app.Run();
