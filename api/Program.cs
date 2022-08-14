af877e8d620dcac8bc7417e895f7762e933b39cb
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-08-14 13:53:07 -0300

    Ajustes

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,15 @@
+using MySql.Data.MySqlClient;
 using Microsoft.Extensions.FileProviders;
 
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+String connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+MySqlConnection conexao = new MySqlConnection(connectionString);
+MySqlCommand comando = new MySqlCommand("SELECT * FROM produto", conexao);
+comando.ExecuteReader();
 
 /*
 var provider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Public"));
