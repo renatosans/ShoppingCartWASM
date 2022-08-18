f0ce047595c2935a8d9c9493cc1df60619fdce1f
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-08-17 23:54:04 -0300

    Alterando o banco de dados para o MySQL

---
+++ api/models/Products.cs
@@ -0,0 +1,22 @@
+
+namespace APIDemo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+        public string nome { get; set; }
+        public decimal preco { get; set; }
+        public string descricao { get; set; }
+        public string foto { get; set; }
+        public string formatoImagem { get; set; }
+        public DateTime dataCriacao { get; set; }
+    }
+
+    class CommerceDB : DbContext
+    {
+        public CommerceDB(DbContextOptions<CommerceDB> options) : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+    }
+
+}
